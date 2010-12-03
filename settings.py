de95af4ef93d3551fbd98b147bb64beb85161c10
Titus Soporan <titus@tsoporan.com>
2010-12-03 12:37:35 -0500

    Path issues fixed.

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ djcelery.setup_loader()
 
 USER_AGENT = getattr(settings, "USER_AGENT", "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.7.6) Gecko/20050512 Firefox")
 
-CELERY_IMPORTS = getattr(settings, "CELERY_IMPORTS", ("kral.plugins.twitter.tasks", "kral.plugins.facebook.tasks"))
+#CELERY_IMPORTS = getattr(settings, "CELERY_IMPORTS", ("kral.plugins.facebook.tasks",))
 
 
 # vim: ai ts=4 sts=4 et sw=4
