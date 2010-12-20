29077bf065930e4e401f26532521daba5fedb308
Lance R. Vick <lance@lrvick.net>
2010-12-20 13:20:32 -0500

    removed redundant KRAL_WAIT checking.

--- views.py
+++ views.py
@@ -8,16 +8,10 @@ from django.conf import settings
 
 def serialize_model(request,model,query,format):
     query_object,created = Query.objects.get_or_create(text=query) 
-    if hasattr(settings, 'KRAL_WAIT') and settings.KRAL_WAIT: 
-        sec = datetime.timedelta(seconds=settings.KRAL_WAIT)
-        if query_object.last_modified + sec <= datetime.datetime.now():
-            query_object.last_modified = datetime.datetime.now()
-            query_object.save() 
-    else:
-        query_object.last_modified = datetime.datetime.now()
-        query_object.save() 
+    query_object.last_modified = datetime.datetime.now()
+    query_object.save() 
     query_object = Query.objects.get(text__iexact=query)
-    results = serializers.serialize(format, query_object.twittertweet_set.all())
+    results = serializers.serialize(format, query_object.twittertweet_set.all()[:1])
     return HttpResponse(results)
 
 
