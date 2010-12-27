a38bc564d24ec30228b594c244502618e269d4b7
Lance R. Vick <lance@lrvick.net>
2010-12-27 15:00:49 -0500

    hardcode feeds for fb/tw/urls

--- urls.py
+++ urls.py
@@ -2,8 +2,7 @@ from django.conf.urls.defaults import *
 from views import *
 
 urlpatterns = patterns('',
-  (r'^(?P<query>\w+).(?P<format>\w+)$', serialize_model),
-  (r'^(?P<model>\w+)/(?P<query>\w+).(?P<format>\w+)$', serialize_model),
+  (r'^(?P<plugin>\w+)/(?P<query>\w+).(?P<format>\w+)$', serialize_model),
 )
 
 # vim: ai ts=4 sts=4 et sw=4
--- views.py
+++ views.py
@@ -7,13 +7,21 @@ from kral.models import *
 from django.conf import settings
 #currently hardcoding for twitter tweets only. will look at get_app / get_models for making it universal.
 
-def serialize_model(request,query,format):
+def serialize_model(request,plugin,query,format):
     query_object,created = Query.objects.get_or_create(text=query) 
     query_object.last_modified = datetime.datetime.now()
     query_object.save() 
     query_object = Query.objects.get(text__iexact=query)
-    #results = serializers.serialize(format, query_object.twittertweet_set.all()[:10])
-    results = serializers.serialize(format, query_object.facebookpost_set.all()[:10])
+    
+    ##hardcoded for now
+    if plugin == 'twitter':
+        results = serializers.serialize(format, query_object.twittertweet_set.all()[:10])
+    if plugin == 'facebook':
+        results = serializers.serialize(format, query_object.facebookpost_set.all()[:10])
+    if plugin == 'weblink':
+        results = serializers.serialize(format, query_object.weblink_set.all()[:10])
+    ## TODO make the above dynamic
+
     return HttpResponse(results)
 
 
