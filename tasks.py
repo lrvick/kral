653934a8555952d92fed3df323f33d937f21de8d
Lance R. Vick <lance@lrvick.net>
2011-04-13 14:28:11 -0400

    make url_expand tasks suicide after x seconds

--- tasks.py
+++ tasks.py
@@ -28,103 +28,109 @@ def kral_init(**kwargs):
     PeriodicTasks.objects.all().delete()
     task_cache = redis.Redis(host='localhost', port=6379, db=0)
     task_cache.flushdb()
+    cache.set('KRAL_QUERIES',settings.KRAL_QUERIES)
 beat_init.connect(kral_init) 
-
-def fixurl(url):
-    domain, query = urllib.splitquery(url)
-    new_url = None
-    if query:
-        query = re.sub('utm_(source|medium|campaign)\=([^&]+)&?', '', query)
-        new_url = urlparse.urljoin(domain, "?"+query)
-    else:
-        new_url = domain 
-    return new_url
 
 @task
 def url_expand(url,query,n=1,original_url=None,**kwargs):
-    if n == 1:
-        original_url = url
     logger = url_expand.get_logger(**kwargs)
-    headers = {"User-Agent": "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.7.6) Gecko/20050512 Firefox"}
-    parsed_url = urlparse.urlsplit(url)
-    request = urlparse.urlunsplit(('', '', parsed_url.path, parsed_url.query, parsed_url.fragment))
-    response = None
-    current_url = None
-    try:
-        connection = httplib.HTTPConnection(parsed_url.netloc)
-    except httplib.InvalidURL:
-        logger.error("Unable to expand Invalid URL: %s" % url)
-        return False
-    with Timeout(5, False) as timeout:
-        try: 
-            connection.request('HEAD', request, "", headers)
-            response = connection.getresponse()
+    def fixurl(url):
+        domain, query = urllib.splitquery(url)
+        new_url = None
+        if query:
+            query = re.sub('utm_(source|medium|campaign)\=([^&]+)&?', '', query)
+            new_url = urlparse.urljoin(domain, "?"+query)
+        else:
+            new_url = domain 
+        return new_url
+    def expand(url,query,n=1,original_url=None,**kwargs):
+        if n == 1:
+            original_url = url
+        headers = {"User-Agent": "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.7.6) Gecko/20050512 Firefox"}
+        parsed_url = urlparse.urlsplit(url)
+        request = urlparse.urlunsplit(('', '', parsed_url.path, parsed_url.query, parsed_url.fragment))
+        response = None
+        current_url = None
+        try:
+            connection = httplib.HTTPConnection(parsed_url.netloc)
+        except httplib.InvalidURL:
+            logger.error("Unable to expand Invalid URL: %s" % url)
+            return False
+            try: 
+                connection.request('HEAD', request, "", headers)
+                response = connection.getresponse()
+            except Exception, e:
+                logger.error(e)
+        if response:
+            location = response.getheader('Location')
+            if location:
+                content_header = response.getheader('Content-Type');
+                if content_header:
+                    encoding = content_header.split('charset=')[-1]
+                    try:
+                        current_url = unicode(location, encoding)
+                    except LookupError:
+                        pass
+        n += 1
+        if n > 3 or current_url == None:
+            cache_name = "alllinks_%s" % str(query.replace(' ',''));
+            try:
+                links = pickle.loads(cache.get(cache_name))
+            except:
+                links = []
+            url = fixurl(url)
+            new_link = False
+            url_cache_name = base64.b64encode(url.encode('unicode-escape'))[:250]
+            cached_title = cache.get(url_cache_name)
+            title = None
+            if cached_title:
+                title = base64.b64decode(cached_title)
+                unicode(title,'utf8')
+            else:
+                title = None
+            for link in links:
+                if link['href'] == url:
+                    link['count'] += 1
+                    if link['count'] > 1:
+                        if  title:
+                            link['title'] = title
+                        else: 
+                            url_title.delay(url)
+                    new_link = True
+                    post_info = link
+            if new_link == False:
+                post_info = {'service':'links','href':url,'count':1,'title':title}
+                links.append(post_info)
+            links = sorted(links, key=lambda link: link['count'],reverse=True)
+            cache.set(cache_name, pickle.dumps(links))
+            push_data(post_info,queue=query)
+        else:
+            expand(current_url,query,n,original_url)
+    with Timeout(10, False) as timeout:
+        try:
+            expand(url,query,n=1,original_url=None,**kwargs)
+        except Timeout:
+            logger.error("URL Timed out: %s" % url)
         except Exception, e:
             logger.error(e)
-        except Timeout:
-            logger.info("URL Timed out: %s" % url)
-    if response:
-        location = response.getheader('Location')
-        if location:
-            content_header = response.getheader('Content-Type');
-            if content_header:
-                encoding = content_header.split('charset=')[-1]
-                try:
-                    current_url = unicode(location, encoding)
-                except LookupError:
-                    pass
-    n += 1
-    if n > 3 or current_url == None:
-        cache_name = "alllinks_%s" % str(query.replace(' ',''));
-        try:
-            links = pickle.loads(cache.get(cache_name))
-        except:
-            links = []
-        url = fixurl(url)
-        new_link = False
-        url_cache_name = base64.b64encode(url)[:250]
-        cached_title = cache.get(url_cache_name)
-        title = None
-        if cached_title:
-            title = base64.b64decode(cached_title)
-            unicode(title,'utf8')
-        else:
-            title = None
-        for link in links:
-            if link['href'] == url:
-                link['count'] += 1
-                if link['count'] > 1:
-                    if  title:
-                        link['title'] = title
-                    else: 
-                        url_title.delay(url)
-                new_link = True
-                post_info = link
-        if new_link == False:
-            post_info = {'service':'links','href':url,'count':1,'title':title}
-            links.append(post_info)
-        links = sorted(links, key=lambda link: link['count'],reverse=True)
-        cache.set(cache_name, pickle.dumps(links))
-        push_data(post_info,queue=query)
-    else:
-        url_expand.delay(current_url,query,n,original_url)
-
+    
 @task
 def url_title(url,**kwargs):
     logger = url_expand.get_logger(**kwargs)
     cache_name = base64.b64encode(url)[:250]
     request = urllib2.Request(url)
+    data = None
     try:
         response = urllib2.urlopen(request)
         data = response.read()
     except urllib2.HTTPError:
-        data = None
+        pass
     except urllib2.URLError:
-        data = None
+        pass
     except httplib.BadStatusLine:
-        data = None
+        pass
     except httplib.InvalidURL:
-        data = None
+        pass
     except httplib.IncompleteRead:
         data = None
     except ValueError:
@@ -149,6 +155,6 @@ def url_title(url,**kwargs):
                         print(e)
                         title = None
             else:
-                loggger.error("Unknown content type for URL: %s" % url)
+                logger.error("Unknown content type for URL: %s" % url)
 
 #vim: ai ts=4 sts=4 et sw=4
