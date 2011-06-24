683f29a769d19390542dc09f0c66fd9db54877c5
Lance R. Vick <lance@lrvick.net>
2011-06-24 15:59:47 -0400

    verifiy results exist before manipulating in stream()

--- __init__.py
+++ __init__.py
@@ -15,17 +15,18 @@ def stream(queries):
                     services[service] = {}
                     services[service]['refresh_url'] = None
                 result = send_task('plugins.%s.%s' % (service,service),[query, services[service]['refresh_url']]).get()
-                services[service]['refresh_url'] = result[0] 
-                taskset = result[1]
-                tasks.extend(taskset.subtasks) 
-                while tasks:
-                    current_task = tasks.pop(0)
-                    if current_task.ready():
-                        post = current_task.get()
-                        if post:
-                            yield post
-                    else:
-                        tasks.append(current_task)
+                if result:
+                    services[service]['refresh_url'] = result[0] 
+                    taskset = result[1]
+                    tasks.extend(taskset.subtasks) 
+                    while tasks:
+                        current_task = tasks.pop(0)
+                        if current_task.ready():
+                            post = current_task.get()
+                            if post:
+                                yield post
+                        else:
+                            tasks.append(current_task)
 
 if __name__ == '__main__':
     for item in stream(['android','bitcoin']):
