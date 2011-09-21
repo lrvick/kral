cb366b71e72853000db14564700b5f1ade268054
lrvick <lance@lrvick.net>
2011-09-21 19:44:33 -0400

    remove redis from setup.py requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
     description='Library and CLI for performing queries against a wide range of social networks, and returning live streaming results in a normalized format',
     long_description=open('README.md').read(),
     install_requires=[
-        'redis',
         'simplejson',
         'eventlet',
         'lxml',
