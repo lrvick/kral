10c274cfcb6140661face6888326f17548ef1253
Titus Soporan <titus@tsoporan.com>
2011-11-14 18:03:55 -0500

    fix facebook time issues, and add kral_start_time

--- kral/services/facebook.py
+++ kral/services/facebook.py
@@ -4,12 +4,11 @@ import time
 import re
 import urllib
 import simplejson as json
-from urlparse import urlparse
 from eventlet.green import urllib2
 from eventlet.greenthread import sleep
-import urllib
 
-def stream(queries, queue, settings):
+def stream(queries, queue, settings, kral_start_time):
+
     def get_access_token():
 
         url_args = {
@@ -25,10 +24,15 @@ def stream(queries, queue, settings):
 
     access_token = get_access_token()
 
-    last_id = None
+    #keep a store of queries and their most recent posts' created times
+    sinces = {}
 
     while True:
         for query in queries:
+
+            print sinces
+            #https://developers.facebook.com/docs/reference/api/batch/
+            #do batch requests for facebook, currently limited to 20
             url_args = {
                 'access_token' : access_token,
                 'batch': [
@@ -45,69 +49,108 @@ def stream(queries, queue, settings):
                 ]
             }
 
-            if last_id:
-                url_args['batch'][0]['relative_url'] = "%s&since=%s" % (url_args['batch'][0]['relative_url'],last_id)
+            #if we have stored a "since" for this query and its newer than when we started
+            #use that timestamp instead, else use the time we started
+            if query in sinces and sinces[query] > kral_start_time:
+                since = sinces[query]
+            else:
+                since = kral_start_time
+            print since
+
+            #set the since to retreive new posts
+            url_args['batch'][0]['relative_url'] = "%s&since=%s" % (url_args['batch'][0]['relative_url'], since)
+            print url_args
 
             url = 'https://graph.facebook.com'
             request = urllib2.Request(url)
             request.add_data(urllib.urlencode(url_args))
-            response = json.loads(urllib2.urlopen(request).read())
+            
+            try:
+                response = json.loads(urllib2.urlopen(request).read())
+            except urllib2.URLError, e:
+                print e
 
-            if len(response) > 1:
-                decoded_body = json.loads(response[0]['body'])
-                profiles = json.loads(response[1]['body'])
-                items = decoded_body['data']
+            if len(response) == 2:
+                
+                #first in response is the posts
+                #second in response is the profiles
+                posts, profiles = response
+                
+                if 'body' in posts and 'body' in profiles:
 
-                if 'paging' in decoded_body:
-                    previous_url = decoded_body['paging']['previous']
-                    previous_url_args = urlparse(previous_url)[4].split('&')
-                    for arg in previous_url_args:
-                        key,value = arg.split('=')
-                        if key == 'since':
-                            since_id = value
-                    if not last_id:
-                        last_id = since_id
-                    elif since_id > last_id:
-                        last_id = since_id
-                for item in items:
-                    if 'message' in item:
-                        post = {
-                            "service" : 'facebook',
-                            "query": query,
-                            "user" : {
-                                "name": item['from'].get('name'),
-                                "id": item['from']['id'],
-                                "subscribers" : '0'
-                            },
-                            "links" : [],
-                            "id" : item['id'],
-                            "text" : item['message'],
-                            "date": int(time.mktime(time.strptime(item['created_time'],'%Y-%m-%jT%H:%M:%S+0000'))),
-                        }
-                        url_regex = re.compile('(?:http|https|ftp):\/\/[\w\-_]+(?:\.[\w\-_]+)+(?:[\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?')
-                        for url in url_regex.findall(item['message']):
-                            post['links'].append({ 'href' : url })
-                        post['user']['avatar'] = "http://graph.facebook.com/%s/picture" % item['from']['id']
-                        if 'to' in item:
-                            post['to_users'] = item['to']['data']
-                        if 'likes' in item:
-                            post['likes'] = item['likes']['count']
-                        subscribers_estimate = 0
-                        if item['from']['id'] in profiles:
-                            activity = 0
-                            for profile_item in profiles[item['from']['id']]['data']:
-                                activity += profile_item['comments']['count']
-                                if 'likes' in profile_item:
-                                    activity += profile_item['likes']['count']
-                            subscribers_estimate = activity * 10
-                        if subscribers_estimate < 130:
-                            post['user']['subscribers'] = 130
-                        else:
-                            post['user']['subscribers'] = subscribers_estimate
-                            # More research needs to be done into making a more accurate multiplier
-                            # what is the rough percentage of total friends someone has vs. how many
-                            # actuall participate on their wall on a regular basis?
-                            # We can only do our best consistant guess, as Facebook does not tell us
-                            # how many friends someone has. We can only guess by activity.
-                        queue.put(post)
-            sleep(1) # Facebook's API maxes out at 1 request/sec
+                    decoded_posts = json.loads(posts['body'])
+                    decoded_profiles = json.loads(profiles['body'])
+
+                    if 'data' in decoded_posts:
+                        items = decoded_posts['data']
+                    else:
+                        items = []
+                    
+                    #set the first items created_time to be our "since" for this query
+                    if items:
+                        print items[0]
+                        created_time = items[0]['created_time']
+                        if created_time > since:
+                            sinces[query] = int(time.mktime(time.strptime(created_time,'%Y-%m-%dT%H:%M:%S+0000')))
+
+                    for item in items:
+                       
+                        created_time = int(time.mktime(time.strptime(item['created_time'],'%Y-%m-%dT%H:%M:%S+0000')))
+                       
+                        #only process if these posts are in fact new
+                        if created_time > since:
+                            print "processing ..."    
+
+                            if 'message' in item:
+                                
+                                post = {
+                                    "service" : 'facebook',
+                                    "query": query,
+                                    "user" : {
+                                        "name": item['from'].get('name'),
+                                        "id": item['from']['id'],
+                                        "subscribers" : '0'
+                                    },
+                                    "links" : [],
+                                    "id" : item['id'],
+                                    "text" : item['message'],
+                                    "date": created_time, 
+                                }
+                                
+                                url_regex = re.compile('(?:http|https|ftp):\/\/[\w\-_]+(?:\.[\w\-_]+)+(?:[\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?')
+                                
+                                for url in url_regex.findall(item['message']):
+                                    post['links'].append({ 'href' : url })
+                                post['user']['avatar'] = "http://graph.facebook.com/%s/picture" % item['from']['id']
+                                
+                                if 'to' in item:
+                                    post['to_users'] = item['to']['data']
+                                
+                                if 'likes' in item:
+                                    post['likes'] = item['likes']['count']
+                                
+                                subscribers_estimate = 0
+                                
+                                if item['from']['id'] in decoded_profiles:
+                                    activity = 0
+                                    
+                                    for profile_item in decoded_profiles[item['from']['id']]['data']:
+                                        activity += profile_item['comments']['count']
+                                        if 'likes' in profile_item:
+                                            activity += profile_item['likes']['count']
+                                    
+                                    subscribers_estimate = activity * 10
+                                
+                                if subscribers_estimate < 130:
+                                    post['user']['subscribers'] = 130
+                                else:
+                                    post['user']['subscribers'] = subscribers_estimate
+                                    # More research needs to be done into making a more accurate multiplier
+                                    # what is the rough percentage of total friends someone has vs. how many
+                                    # actuall participate on their wall on a regular basis?
+                                    # We can only do our best consistant guess, as Facebook does not tell us
+                                    # how many friends someone has. We can only guess by activity.
+                                queue.put(post)
+
+                    
+            sleep(2) # time between requests this is 2 * num_queries
